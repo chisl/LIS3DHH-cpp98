eee7db9d2b75b6f9d9999750e619999f577fef69
chisl <info@chisl.io>
2018-01-27 08:43:22 +0100

    Initial commit

---
+++ LIS3DHH.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        LIS3DHH
+ * description: MEMS digital output motion sensor ultra low-power high performance 3-axes 'nano' accelerometer
+ * manuf:       ST Microelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/lis3dhh.pdf
+ * date:        2018-01-26
+ * author       https://chisl.io/
+ * file:        LIS3DHH.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "LIS3DHH.hpp"
+
